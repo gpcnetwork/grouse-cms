c0edbfd1c48a49398a4bc85a41210f6824ce9898
Song <xsm7f@umsystem.edu>
2022-10-03 10:55:19 -0500

    update c2p scripts

--- src/c2p/ddl/demographic_stg_ddl.sql
+++ src/c2p/ddl/demographic_stg_ddl.sql
@@ -17,5 +17,4 @@ create or replace table PRIVATE_DEMOGRAPHIC_STAGE (
     ,SRC_SCHEMA varchar(20) NOT NULL
     ,SRC_TABLE varchar(30) NOT NULL
     ,SRC_DATE date NOT NULL
-    ,SRC_CONFIDENCE integer NOT NULL -- highest confidence to MBSF table
 );
